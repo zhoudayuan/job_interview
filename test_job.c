893e99218accd0bc72b8dc219961bc3e63da2ece
zhoudayuan <yigangkegang@126.com>
2016-06-23 20:34:48 +0800

    str_len

--- test_job.c
+++ test_job.c
@@ -4,6 +4,10 @@
 char *str_cpy(char*	dest, const	char *src);
 char *str_n_cpy(char *dest,	const char *src, unsigned int n);
 char *str_n_cpy_0(char *dest,	const char *src, unsigned int n);
+int str_len(const char *src);
+
+
+
 
 int	main()
 {
@@ -11,7 +15,8 @@ int	main()
 	printf("[%s:%d]	~~~~~~~~~ copy str ~~~~~~~~~\n", __FUNCTION__, __LINE__);
 	printf("[%s:%d]	array=%s\n", __FUNCTION__, __LINE__, str_cpy(array,	"zhoudayuan-abc"));
 	printf("[%s:%d]	array=%s\n", __FUNCTION__, __LINE__, str_n_cpy(array, "abcd", 10));
-	printf("[%s:%d]	array=%s\n", __FUNCTION__, __LINE__, str_n_cpy(array, "123456789", 10));
+	printf("[%s:%d]	array=%s\n", __FUNCTION__, __LINE__, str_n_cpy(array, "123456789", 5));
+	printf("[%s:%d]	len=%d\n", __FUNCTION__, __LINE__, str_len(array));
 	return 0;
 }
 
@@ -68,3 +73,19 @@ char *str_n_cpy_0(char *dest,	const char *src, unsigned int n)
 
 	return dest;
 }
+
+
+
+/*
+ * ×Ö·û´®³¤¶È
+ */
+int str_len(const char *src)
+{
+	int len = 0;
+	while (*src++ != '\0')
+	{
+		len++;
+	}
+	return len;
+}
+
